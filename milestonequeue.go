c826bc1a76c25d2f1b9c687cb34f19ca32676245
Tetsu Takizawa <tetsu.takizawa5@gmail.com>
2020-09-04 14:53:26 +0900

    Add milestone queue

---
+++ milestonequeue.go
@@ -0,0 +1,36 @@
+package ruleanalyzer
+
+import "go/types"
+
+type MilestoneQueue []types.Object
+
+func (q *MilestoneQueue) Push(t types.Object) {
+	*q = append(*q, t)
+}
+
+func (q *MilestoneQueue) Pop() (t types.Object) {
+	if q == nil {
+		return nil
+	}
+	t = (*q)[0]
+	*q = (*q)[1:]
+	return t
+}
+
+func (q *MilestoneQueue) Head() types.Object {
+	if q.Len() == 0 {
+		return nil
+	}
+	return (*q)[0]
+}
+
+func (q *MilestoneQueue) Tail() types.Object {
+	if q.Len() == 0 {
+		return nil
+	}
+	return (*q)[(*q).Len()-1]
+}
+
+func (q *MilestoneQueue) Len() int {
+	return len(*q)
+}
